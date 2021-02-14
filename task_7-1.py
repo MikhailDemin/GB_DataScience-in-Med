4923731dff61583ac9473a47d8cc0423fa17f83c
MikhailDemin <bahamutffxii@gmail.com>
2021-02-14 10:55:18 +1000

    lesson 7 tasks add + tutor solutions

--- task_7-1.py
+++ task_7-1.py
@@ -35,3 +35,91 @@ matrix_1 = Matrix(a)
 matrix_2 = Matrix(b)
 print(f'Matrix 1\n{matrix_1}\n{"-" * 20}')
 print(f'Matrix 2\n{matrix_2}\n{"-" * 20}')
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+class Matrix:
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return '\n'.join('\t'.join([str(itm) for itm in line]) for line in self.data)
+
+    def __add__(self, other):
+        try:
+            m = [[int(self.data[line][itm]) + int(other.data[line][itm]) for itm in range(len(self.data[line]))]
+                 for line in range(len(self.data))]
+            return Matrix(m)
+        except IndexError:
+            return f'Ошибка размерностей матриц'
+
+
+m_1 = [[3, 5, 32], [2, 4, 6], [-1, 64, -8]]
+m_2 = [['5', '7', '23'], ['9', '23', '-54'], ['12', '3', '16']]
+
+mtrx_1 = Matrix(m_1)
+mtrx_2 = Matrix(m_2)
+new_m = mtrx_1 + mtrx_2
+
+print(mtrx_1)
+print('#' * 30)
+print(mtrx_2)
+print('#' * 30)
+print(mtrx_1 + mtrx_2)
+print('#' * 30)
+print(new_m)
+m_3 = [[3, 5, 32], [2, 4, 6], [-1, 64, -8]]
+m_4 = [['5', '7', '23'], ['9', '-54'], ['12', '3', '16']]
+print('#' * 30)
+mtrx_3 = Matrix(m_3)
+mtrx_4 = Matrix(m_4)
+print(mtrx_3 + mtrx_4)
+print('#' * 30)
+m_3 = [[3, 5, 32], [2, 4, 6], [-1, 64, -8]]
+m_4 = [['5', '7', '23'], ['12', '3', '16'], ['12', '3']]
+print(mtrx_3 + mtrx_4)
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+class Matrix(object):
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        return str('\n '.join(['\t '.join([str(i) for i in j]) for j in self.matrix]))
+
+    def __add__(self, other):
+        return Matrix([map(sum, zip(*t)) for t in zip(self.matrix, other.matrix)])
+
+
+stroki = int(input("Введите количество строк и столбцов матрицы: "))
+stolbci = stroki
+
+matrix1 = Matrix([[i * j for j in range(stroki)] for i in range(stolbci)])
+matrix2 = Matrix([[i + j for j in range(stroki)] for i in range(stolbci)])
+
+print('First matrix:\n', matrix1, end='\n\n')
+print('Second matrix:\n', matrix2, end='\n\n')
+print('Summ of first and second matrix:\n', matrix1 + matrix2)
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        return '\n'.join(map(lambda r: '   '.join(map(str, r)), self.matrix)) + '\n'
+
+    def __add__(self, other):
+        return Matrix(map(lambda r_1, r_2: map(lambda x, y: x + y, r_1, r_2), self.matrix, other.matrix))
+
+
+my_m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+my_m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(my_m1)
+print(my_m2)
+s = my_m1 + my_m2
+print(s)
--- task_7-3.py
+++ task_7-3.py
@@ -17,4 +17,38 @@
 # Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5. Тогда метод make_order() вернет
 # строку: *****\n*****\n*****.
 # Подсказка: подробный список операторов для перегрузки доступен по ссылке
-# my solution ####
+class Cell:
+    def __init__(self, nums):
+        self.nums = nums  # 13
+
+    def make_order(self, rows):  # 5
+        return '\n'.join(['*' * rows for _ in range(self.nums // rows)]) + '\n' + '*' * (self.nums % rows)
+
+    def __str__(self):
+        return f"{self.nums}"
+
+    def __add__(self, other):
+        print("Sum of cell is:")
+        return Cell(self.nums + other.nums)
+
+    def __sub__(self, other):
+        print("Subtraction of cell is:")
+        return Cell(self.nums - other.nums) if self.nums - other.nums > 0 \
+            else "Ячеек в первой клетке меньше второй, вычитание невозможно!"
+
+    def __mul__(self, other):
+        print("Multiply of cell is:")
+        return Cell(self.nums * other.nums)
+
+    def __floordiv__(self, other):
+        print("Truediv of cell is:")
+        return Cell(self.nums // other.nums)
+
+
+cell_1 = Cell(15)
+cell_2 = Cell(24)
+print(cell_1 + cell_2)
+print(cell_1 - cell_2)
+print(cell_1 * cell_2)
+print(cell_1 // cell_2)
+print(cell_2.make_order(7))
