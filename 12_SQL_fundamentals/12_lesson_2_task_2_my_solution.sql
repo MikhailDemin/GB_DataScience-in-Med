74837037f0f30412fd56404a2707fc5c45ff853c
Mikhail Demin <64045335+MikhailDemin@users.noreply.github.com>
2022-02-20 03:36:09 +1000

    12_lesson_2_solution_files_update

--- 12_SQL_fundamentals/12_lesson_2_task_2_my_solution.sql
+++ 12_SQL_fundamentals/12_lesson_2_task_2_my_solution.sql
@@ -4,8 +4,8 @@ USE example;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) DEFAULT 'user' COMMENT 'имя пользователя'
+	id INT SERIAL PRIMARY KEY,
+	name VARCHAR(255) DEFAULT 'user' COMMENT 'пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ'
 );
 INSERT INTO users (name) VALUES ('developer');
 INSERT INTO users (name) VALUES ('user_1'), ('admin'), (DEFAULT);
