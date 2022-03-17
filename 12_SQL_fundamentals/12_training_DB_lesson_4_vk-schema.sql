ddba47f4d0c54c1445ce74a49796ccf1cc9a8cdf
Mikhail Demin <64045335+MikhailDemin@users.noreply.github.com>
2022-03-18 01:55:33 +1000

    12_lesson_4_my_solution_file_add

--- 12_SQL_fundamentals/12_training_DB_lesson_4_vk-schema.sql
+++ 12_SQL_fundamentals/12_training_DB_lesson_4_vk-schema.sql
@@ -7,21 +7,21 @@ CREATE TABLE users (
 	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	-- id SERIAL,
 	-- id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-	firstname VARCHAR(100) COMMENT 'РРјСЏ',
-	lastname VARCHAR(100) COMMENT 'Р¤Р°РјРёР»РёСЏ',
+	firstname VARCHAR(100) COMMENT 'Имя',
+	lastname VARCHAR(100) COMMENT 'Фамилия',
 	email VARCHAR(100) UNIQUE,
 	password_hash VARCHAR(256),
-	phone BIGINT UNSIGNED UNIQUE COMMENT 'РўРµР»РµС„РѕРЅ', -- +7(999) 123-45-67 => 79 991 234 567
+	phone BIGINT UNSIGNED UNIQUE COMMENT 'Телефон', -- +7(999) 123-45-67 => 79 991 234 567
 	
 	INDEX idx_firstname_lastname(firstname, lastname)
-) COMMENT 'РџРѕР»СЊР·РѕРІР°С‚РµР»Рё';
+) COMMENT 'Пользователи';
 
 -- 1 x 1
 DROP TABLE IF EXISTS profiles;
 CREATE TABLE profiles (
 	user_id BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-	gender CHAR(1) COMMENT 'РџРѕР»',
-	hometown VARCHAR(200) COMMENT 'Р РѕРґРЅРѕР№ РіРѕСЂРѕРґ',
+	gender CHAR(1) COMMENT 'Пол',
+	hometown VARCHAR(200) COMMENT 'Родной город',
 	created_at DATETIME DEFAULT NOW()
 );
 
@@ -104,7 +104,7 @@ CREATE TABLE media (
 	-- media_type ENUM('text', 'video', 'music', 'image'),
 	media_type_id BIGINT UNSIGNED NOT NULL,
 	body VARCHAR(255),
-	-- file BLOB, -> РїСЂРёРІРµРґРµС‚ Рє СѓРІРµР»РёС‡РµРЅРёСЋ СЂР°Р·РјРµСЂР° Р±Р°Р·С‹ Рё РїСЂРѕР±Р»РµРјР°Рј СЃ РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЊРЅРѕСЃС‚СЊСЋ
+	-- file BLOB, -> приведет к увеличению размера базы и проблемам с производительностью
 	filename VARCHAR(255),
 	metadata JSON,
 	created_at DATETIME DEFAULT NOW(),
@@ -122,4 +122,60 @@ CREATE TABLE likes (
 	created_at DATETIME DEFAULT NOW()
 );
 
+ALTER TABLE likes
+ADD CONSTRAINT fk_likes_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+ADD CONSTRAINT fk_likes_media_id FOREIGN KEY (media_id) REFERENCES media(id)
+;
 
+DROP TABLE IF EXISTS links;
+CREATE TABLE links (
+	id SERIAL
+);
+
+
+-- таблица компаний, которые представлены в соц сети, а также пользователи сети могут быть
+-- сотрудниками этих кампаний
+DROP TABLE IF EXISTS companies;
+CREATE TABLE companies (
+	id SERIAL,
+	created_at DATETIME DEFAULT NOW(),
+	company_site BIGINT UNSIGNED NOT NULL,
+	employee_id BIGINT UNSIGNED NOT NULL,
+	
+	FOREIGN KEY (employee_id) REFERENCES users(id),
+	FOREIGN KEY (company_site) REFERENCES links(id)
+);
+
+ALTER TABLE profiles ADD COLUMN employee_of_company BIGINT UNSIGNED NOT NULL;
+ALTER TABLE profiles ADD CONSTRAINT fk_profiles_company_id
+FOREIGN KEY (employee_of_company) REFERENCES companies(id)
+;
+
+ALTER TABLE links
+ADD COLUMN media_id BIGINT UNSIGNED NOT NULL,
+ADD COLUMN user_id BIGINT UNSIGNED NOT NULL,
+ADD COLUMN body VARCHAR(255),
+ADD COLUMN name VARCHAR(255),
+ADD COLUMN created_at DATETIME DEFAULT NOW(),
+ADD COLUMN updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+ADD CONSTRAINT fk_links_media_id FOREIGN KEY (media_id) REFERENCES media(id),
+ADD CONSTRAINT fk_links_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+;
+
+-- таблица звонков пользователей
+DROP TABLE IF EXISTS calls;
+CREATE TABLE calls (
+	id SERIAL,
+	initiator_user_id BIGINT UNSIGNED NOT NULL,
+	target_user_id BIGINT UNSIGNED NOT NULL,
+	started_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	finished_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+	
+	PRIMARY KEY (initiator_user_id, target_user_id), 
+	FOREIGN KEY (initiator_user_id) REFERENCES users(id),
+	FOREIGN KEY (target_user_id) REFERENCES users(id),
+	CHECK (initiator_user_id != target_user_id)
+);
+
+ALTER TABLE profiles ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT(TRUE);
+ALTER TABLE profiles ADD COLUMN age TINYINT UNSIGNED NOT NULL;
